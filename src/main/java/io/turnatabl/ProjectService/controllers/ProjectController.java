ea2036e6ffa9947ebf4b1d114c11a75a8b40f766
bill <teebill15@gmail.com>
2019-12-12 16:40:06 +0100

    file modified

--- src/main/java/io/turnatabl/ProjectService/controllers/ProjectController.java
+++ src/main/java/io/turnatabl/ProjectService/controllers/ProjectController.java
@@ -66,7 +66,7 @@ public class ProjectController implements ProjectDAO {
 
     @ApiOperation("Add a project")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping("/Project/addproject")
+    @PostMapping("/project")
     @Override
     public void addProject(@RequestBody Project project) {
        this.jdbcTemplate.update("insert into projects (title, description ) values (?,?)", project.getTitle(), project.getDescription());
