56c70da58c73c63c1ee1f03d277b252cf47ff814
FrancisBilla <francis.billa.fb@gmail.com>
2020-01-15 10:56:29 +0000

    ref 1

--- src/main/java/io/turnatabl/ProjectService/controllers/ProjectController.java
+++ src/main/java/io/turnatabl/ProjectService/controllers/ProjectController.java
@@ -132,7 +132,7 @@ public class ProjectController implements ProjectDAO {
     @Override
     public List<Project> assignedProjectByEmpId(@PathVariable Integer emp_id) {
         return this.jdbcTemplate.query("SELECT * from projects INNER JOIN currentprojects ON currentprojects.project_id = projects.project_id INNER JOIN employees ON currentprojects.emp_id = employees.emp_id where employees.emp_id = ?",
-                new Object[]{emp_id + "%"},
+                new Object[]{emp_id},
                 BeanPropertyRowMapper.newInstance(Project.class));
     }
 
